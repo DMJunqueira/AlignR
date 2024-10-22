c9954c50eff8c7d19b12ed3f7da8416ba505a217
DMJunqueira <92556574+DMJunqueira@users.noreply.github.com>
2024-10-22 18:36:57 -0300

    Update 002R008_ColetandoSequenciasDeArquivoFasta_V1.R

--- 002R008_ColetandoSequenciasDeArquivoFasta_V1.R
+++ 002R008_ColetandoSequenciasDeArquivoFasta_V1.R
@@ -4,13 +4,13 @@
 library(seqinr)
 
 # SETTING A FOLDER
-setwd(dir = "G:/Meu Drive/Pesquisa/2023/HIV/CadeiasSubtipoC/")
+setwd(dir = "../")
 
 # LOAD A FASTA FILE 
-fasta <- read.fasta(file = "02_AlinhamentoTrabalho/NA_01_v9_IAV_Transmissions_AlinhamentoFinal.fas", whole.header = TRUE)
+fasta <- read.fasta(file = "alignment.fas", whole.header = TRUE)
 
 # LOAD A CSV FILE INCLUDING ONLY THE NAMES OF THE SEQUENCES
-list <- read.csv(file = "06_AnaliseSelecao/02_SLAC/05_AllSwineMenosBrazil/NA/NA_SwinemenosBrazil.csv", sep = ";", header = FALSE, skip = 0, check.names = TRUE, blank.lines.skip = TRUE)
+list <- read.csv(file = "df.csv", sep = ";", header = FALSE, skip = 0, check.names = TRUE, blank.lines.skip = TRUE)
 
 # JUST TO CHECK IF EVERTHYNG IS OK 
 names(fasta) %in% list$V1
@@ -18,5 +18,5 @@ names(fasta) %in% list$V1
 # SAVING THE SEQUENCES TO A VARIABLE NAMED CHOSEN
 chosen <- fasta[names(fasta) %in% list$V1]
 write.fasta(sequences = chosen, names = names(chosen), 
-            file.out = "06_AnaliseSelecao/02_SLAC/05_AllSwineMenosBrazil/NA/NA_SwinemenosBrazil.fas")
+            file.out = "newalignment.fas")
             
